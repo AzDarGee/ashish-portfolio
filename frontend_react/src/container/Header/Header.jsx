19b8ac888ac4813e15043e22d4d45555556c7da2
Ashish Darji <ashdarji1@gmail.com>
2023-09-23 13:45:02 -0400

    stlying the header

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -4,9 +4,20 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div id="home" className='app__header app__flex'>
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -34,10 +45,25 @@ const Header = () => {
         className='app__header-img'
       >
         <img src={ images.profile } alt="profile_bg" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className='overlay_circle'
+          src={images.circle}
+          alt='profile_circle'
+        />
       </motion.div>
 
-      <motion.div>
-        
+      <motion.div
+        variant={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className='app__header-circles'
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className='circle-cmp app__flex' key={'circle-${index}'}>
+            <img src={circle} alt='circle' />
+          </div>
+        ))}
       </motion.div>
     </div>
   )
