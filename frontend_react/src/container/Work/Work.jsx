64bc3888ca1e6059a83a641e9fbf8ec3b43cc7d9
Ashish Darji <ashdarji1@gmail.com>
2023-09-24 18:11:13 -0400

    2:15:30 - creative portfolio section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -7,6 +7,13 @@ import { urlFor, client } from '../../client';
 import './Work.scss';
 
 const Work = () => {
+  const [activeFilter, setActiveFilter] = useState('All'); 
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 })
+
+  const handleWorkFilter = (item) => {
+    
+  }
+
   return (
     <>
       <h2 className='head-text'>
@@ -18,11 +25,20 @@ const Work = () => {
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
           >
-          
+            {item}
           </div>
         ))}
       </div>
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__work-portfolio'
+      >
+
+      </motion.div>
     </>
   )
 }
