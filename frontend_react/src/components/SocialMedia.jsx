03a04db9706c96ff56c1f49cf49e251e9c867d43
Ashish Darji <ashdarji1@gmail.com>
2023-12-14 15:24:59 -0500

    updated github icon and link

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+import { FaFacebookF, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className='app__social'>
+      <div>
+        <a href="https://github.com/AzDarGee" target="_blank">
+          <FaGithub />
+        </a>
+      </div>
       <div>
         <a href="https://www.linkedin.com/in/AshDarji/" target="_blank">
           <FaLinkedinIn />
